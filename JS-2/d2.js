0ede2b3f33fbc61fa6a956b3b0ede7cd269a961e
Dinesh R <144953120+16Dinesh@users.noreply.github.com>
2024-05-23 22:31:56 +0530

    Add files via upload

---
+++ JS-2/d2.js
@@ -0,0 +1,15 @@
+// console.log("hello")
+//  hello
+
+// console.log(2+3)
+//  5
+
+// let num = 400
+
+// console.log(num)
+//  400
+
+// console.log("hello", "gandu" , 69)
+//  hello gandu 69
+
+// // console.log();
---
+++ JS-2/d2_1.js
@@ -0,0 +1,27 @@
+// console.log("hello js");
+
+// let num = 2 +5;
+// console.log("total number :", num);
+
+
+// this the genral way 
+// let car=1;
+// let bike=1;
+// console.log("the total :", car + bike , "/-");
+
+
+// template literals
+
+// let car = 200000;
+// let bike = 10000;
+// let output = `the total of both:${car + bike} ruppes`;
+// console.log(output);
+
+//or
+
+// let car = 200000;
+// let bike = 10000;
+// console.log(`the total of both:${car + bike} ruppes`);
+
+
+
